92c3f25d11af54fcb72f109905923983432d2a29
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-12-15 23:56:32 +0530

    Added icon and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:Nirvana/Screens/LandingScreen.dart';
+import 'package:Nirvana/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:Nirvana/Screens/Home.dart';
-
+import 'dart:async';
 void main() async {
   runApp(Main());
 }
@@ -11,8 +13,101 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Nirvana()
+      home: SplashScreen()
     );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  final Color backgroundColor = Colors.white;
+  final TextStyle styleTextUnderTheLoader = TextStyle(
+      fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black);
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  String _versionName = 'v 0.0.1';
+  final splashDelay = 3;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadWidget();
+  }
+
+  _loadWidget() async {
+    var _duration = Duration(seconds: splashDelay);
+    return Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => Nirvana()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: primaryColor,
+              image: DecorationImage(
+                  image: AssetImage("assets/images/home.jpg"),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topCenter),
+            ),
+            child: null),
+        Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(0, 152, 218, 0.85),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 5, right: 5),
+                      child: Icon(
+                        FlutterIcons.home_ant,
+                        color: white,
+                        size: 40,
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: 0),
+                        child: RichText(
+                          text: TextSpan(
+                            semanticsLabel: "Nirvana",
+                            text: 'Nirvana \n',
+                            style: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                height: 1.15),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: 'Rental Made Easy',
+                                  style: TextStyle(
+                                      color: Colors.grey[350],
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.normal)),
+                            ],
+                          ),
+                        ))
+                  ],
+                ),
+              ],
+            )),
+      ],
+    ));
   }
 }
 
